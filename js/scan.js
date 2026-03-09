372983a082fe2dedec67e70c0c1847fd41b79841
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-09 17:13:15 +0900

    feat: カメラ起動

---
+++ js/scan.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", () => {
+    Quagga.init(
+        {
+            inputStream: {
+                type: "LiveStream",
+                target: document.querySelector("#camera"),
+                constraints: {
+                    facingMode: "environment", // 背面カメラ
+                },
+            },
+            decoder: {
+                readers: ["ean_reader"], // 商品バーコード
+            },
+        },
+        function (err) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            Quagga.start();
+        },
+    );
+
+    // 読み取り成功
+    Quagga.onDetected((data) => {
+        const code = data.codeResult.code;
+
+        document.querySelector("#result").textContent = "読み取り成功：" + code;
+
+        console.log(code);
+
+        Quagga.stop();
+    });
+});
