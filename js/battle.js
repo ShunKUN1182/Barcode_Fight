1ee3cea5fff27236f8ef705c339e028001670f6e
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-10 18:20:44 +0900

    feat: 画像とか色々追加

--- js/battle.js
+++ js/battle.js
@@ -6,11 +6,17 @@ const secondATK = document.querySelector("#secondATKStats");
 const secondSPD = document.querySelector("#secondSPDStats");
 const firstHPBar = document.querySelector("#firstHPBar");
 const secondHPBar = document.querySelector("#secondHPBar");
+const firstHPText = document.querySelector("#firstHPtext");
+const secondHPText = document.querySelector("#secondHPtext");
 
 const firstStatus = JSON.parse(localStorage.getItem("firstStatus"));
 const secondStatus = JSON.parse(localStorage.getItem("secondStatus"));
 const firstMaxHP = firstStatus.HP;
 const secondMaxHP = secondStatus.HP;
+
+const firstSPDTime = (10000 - firstStatus.SPD) / 10;
+const secondSPDTime = (10000 - secondStatus.SPD) / 10;
+console.log(firstSPDTime, secondSPDTime);
 
 function outputStatus() {
     firstHP.textContent = firstStatus.HP;
@@ -19,29 +25,35 @@ function outputStatus() {
     secondHP.textContent = secondStatus.HP;
     secondATK.textContent = secondStatus.ATK;
     secondSPD.textContent = secondStatus.SPD;
+    firstHPText.textContent = firstStatus.HP;
+    secondHPText.textContent = secondStatus.HP;
 }
 outputStatus();
 
 const firstTimeID = setInterval(() => {
     secondStatus.HP -= firstStatus.ATK / 10;
     outputHP(secondStatus.HP, "second");
-    console.log(secondStatus.HP);
-}, 1000);
+}, firstSPDTime);
 
 const secondTimeID = setInterval(() => {
-    firstStatus.HP = secondStatus.ATK / 10;
-}, 2000);
+    firstStatus.HP -= secondStatus.ATK / 10;
+    outputHP(firstStatus.HP, "first");
+}, secondSPDTime);
 
 function outputHP(HP, ATKside) {
     if (ATKside == "first") {
         if (HP <= 0) {
             firstHPBar.style.width = `0%`;
+            clearInterval(firstTimeID);
+            clearInterval(secondTimeID);
         }
         const parseHP = (HP / firstMaxHP) * 100;
         firstHPBar.style.width = `${parseHP}%`;
     } else if (ATKside == "second") {
         if (HP <= 0) {
             secondHPBar.style.width = `0%`;
+            clearInterval(firstTimeID);
+            clearInterval(secondTimeID);
         }
         const parseHP = (HP / secondMaxHP) * 100;
         secondHPBar.style.width = `${parseHP}%`;
