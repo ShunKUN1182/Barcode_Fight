fd041570a75a46e8adeb3c49a45be2b6241a7a40
ShunKUN1182 <2250378@ecc.ac.jp>
2026-03-10 17:50:20 +0900

    feat: HPの増減

--- js/battle.js
+++ js/battle.js
@@ -4,10 +4,13 @@ const firstSPD = document.querySelector("#firstSPDStats");
 const secondHP = document.querySelector("#secondHPStats");
 const secondATK = document.querySelector("#secondATKStats");
 const secondSPD = document.querySelector("#secondSPDStats");
+const firstHPBar = document.querySelector("#firstHPBar");
+const secondHPBar = document.querySelector("#secondHPBar");
 
 const firstStatus = JSON.parse(localStorage.getItem("firstStatus"));
 const secondStatus = JSON.parse(localStorage.getItem("secondStatus"));
-console.log(firstStatus, secondStatus);
+const firstMaxHP = firstStatus.HP;
+const secondMaxHP = secondStatus.HP;
 
 function outputStatus() {
     firstHP.textContent = firstStatus.HP;
@@ -18,3 +21,31 @@ function outputStatus() {
     secondSPD.textContent = secondStatus.SPD;
 }
 outputStatus();
+
+const firstTimeID = setInterval(() => {
+    secondStatus.HP -= firstStatus.ATK / 10;
+    outputHP(secondStatus.HP, "second");
+    console.log(secondStatus.HP);
+}, 1000);
+
+const secondTimeID = setInterval(() => {
+    firstStatus.HP = secondStatus.ATK / 10;
+}, 2000);
+
+function outputHP(HP, ATKside) {
+    if (ATKside == "first") {
+        if (HP <= 0) {
+            firstHPBar.style.width = `0%`;
+        }
+        const parseHP = (HP / firstMaxHP) * 100;
+        firstHPBar.style.width = `${parseHP}%`;
+    } else if (ATKside == "second") {
+        if (HP <= 0) {
+            secondHPBar.style.width = `0%`;
+        }
+        const parseHP = (HP / secondMaxHP) * 100;
+        secondHPBar.style.width = `${parseHP}%`;
+    } else {
+        return;
+    }
+}
